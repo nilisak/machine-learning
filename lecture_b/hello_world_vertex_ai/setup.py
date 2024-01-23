8196ad8af263143cb7d4ea8540f28d55a3ba95c0
nilisak <nilisak@chalmers.se>
2024-01-23 18:45:37 +0100

    commit

--- lecture_b/hello_world_vertex_ai/setup.py
+++ lecture_b/hello_world_vertex_ai/setup.py
@@ -6,6 +6,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["lightning==2.1.2"],
+    install_requires=["lightning==2.1.2", "torchvision==0.14.0"],
     description="Hello World",
 )
